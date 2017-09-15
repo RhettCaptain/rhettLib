647191b877f5ba8bcc7125a72f53907adb9e2e6b
rhettcaptain <425237277@qq.com>
2017-09-15 23:21:20 +0800

    c++/

--- c++/tinyxml2/example/createXml.cpp
+++ c++/tinyxml2/example/createXml.cpp
@@ -1,7 +1,8 @@
 #include "../tinyxml2.h"
+#include <iostream>
 
 using namespace tinyxml2;
-
+using namespace std;
 int createXML(const char* xmlPath){
 	const char* dec1 = "<im dec>";
 	XMLDocument doc;
@@ -10,15 +11,30 @@ int createXML(const char* xmlPath){
 	XMLDeclaration* dec2 = doc.NewDeclaration();
 	doc.InsertFirstChild(dec2);
 	
-	XMLElement* root= doc.NewElement("im ele");
+	XMLElement* root= doc.NewElement("imele");
 	doc.InsertEndChild(root);
 
 	return doc.SaveFile(xmlPath);
 }
 
 int insertXMLNode(const char* xmlPath){
-	XML
+	XMLDocument doc;
+	int res=doc.LoadFile(xmlPath);
+	if(res!=0){
+		cout << "load file failed" << endl;
+		return res;
+	}
+	XMLElement* root = doc.RootElement();
+	XMLElement* userNode = doc.NewElement("User");
+	userNode->SetAttribute("Name","rhett");
+	userNode->SetAttribute("Password","123");
+	root->InsertEndChild(userNode);
+		
+	XMLElement* r = doc.NewElement("rr");
+	doc.InsertEndChild(r);
+	return doc.SaveFile(xmlPath);
 }
 int main(){
 	createXML("create.xml");
+	insertXMLNode("create.xml");
 }
